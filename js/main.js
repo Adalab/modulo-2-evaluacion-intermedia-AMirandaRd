d21390abfd7f46596ce80516f08dda73f7b1be5d
Amirandard <am.miranda.rd@gmail.com>
2021-09-01 09:46:57 +0200

    Complete first part of JS

---
+++ js/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const inputNumber = document.querySelector('.js-input');
+const cluetext = document.querySelector('.js-clue');
+const numberOfAttempts = document.querySelector('.js-counter');
+const button = document.querySelector('.js-button');
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * 100);
+}
+const randomNumber = getRandomNumber();
+console.log(randomNumber);
+
+function checkTheNumber() {
+    const selectedNumber = parseInt(inputNumber.value);
+    console.log(selectedNumber);
+
+    if (selectedNumber < 1 || selectedNumber > 100) {
+        cluetext.innerHTML = 'El número debe estar entre 1 y 100'
+    }
+
+    else if (selectedNumber === randomNumber) {
+        cluetext.innerHTML = 'Has ganado campeona!!!'
+    }
+    else if (selectedNumber < randomNumber) {
+        cluetext.innerHTML = 'Demasiado bajo'
+    }
+    else if (selectedNumber > randomNumber) {
+        cluetext.innerHTML = 'Demasiado alto'
+    }
+
+}
+
+// "container" function
+const handlerButton = (event) => {
+    event.preventDefault();
+    checkTheNumber();
+    //attemptsCounter();
+}
+
+
+button.addEventListener('click', handlerButton)
+
+
+
+
