5e9ed846860e2062e199e24574efa1e561dc3966
Amirandard <am.miranda.rd@gmail.com>
2021-09-01 10:26:17 +0200

    Change CSS a little bit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const inputNumber = document.querySelector('.js-input');
-const cluetext = document.querySelector('.js-clue');
-const numberOfAttempts = document.querySelector('.js-counter');
-const button = document.querySelector('.js-button');
+const inputNumber = document.querySelector(".js-input");
+const cluetext = document.querySelector(".js-clue");
+const numberOfAttempts = document.querySelector(".js-counter");
+const button = document.querySelector(".js-button");
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * 100);
@@ -16,38 +16,27 @@ function checkTheNumber() {
     console.log(selectedNumber);
 
     if (selectedNumber < 1 || selectedNumber > 100) {
-        cluetext.innerHTML = 'El número debe estar entre 1 y 100'
+        cluetext.innerHTML = "El número debe estar entre 1 y 100";
+    } else if (selectedNumber === randomNumber) {
+        cluetext.innerHTML = "Has ganado campeona!!!";
+    } else if (selectedNumber < randomNumber) {
+        cluetext.innerHTML = "Demasiado bajo";
+    } else if (selectedNumber > randomNumber) {
+        cluetext.innerHTML = "Demasiado alto";
     }
-
-    else if (selectedNumber === randomNumber) {
-        cluetext.innerHTML = 'Has ganado campeona!!!'
-    }
-    else if (selectedNumber < randomNumber) {
-        cluetext.innerHTML = 'Demasiado bajo'
-    }
-    else if (selectedNumber > randomNumber) {
-        cluetext.innerHTML = 'Demasiado alto'
-    }
-
 }
 
-let counter=0;
+let counter = 0;
 const attemptsCounter = () => {
     counter++;
-    numberOfAttempts.innerHTML= `Número de intentos: ${counter}`
-}
-
+    numberOfAttempts.innerHTML = `Número de intentos: ${counter}`;
+};
 
 // "container" function
 const handlerButton = (event) => {
     event.preventDefault();
     checkTheNumber();
     attemptsCounter();
-}
+};
 
-
-button.addEventListener('click', handlerButton)
-
-
-
-
+button.addEventListener("click", handlerButton);
